2672a4d1e7cc356c92fe0c3cecbb035284decf75
kleingeist37 <seiten@awiworks.com>
2023-01-19 16:41:53 +0100

    code formatting

--- src/app/components/multi-select/multi-select.component.ts
+++ src/app/components/multi-select/multi-select.component.ts
@@ -1,4 +1,4 @@
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
@@ -10,37 +10,33 @@ import { SelectModel } from 'src/app/shared/interfaces/select-model';
 @Component({
   selector: 'app-multi-select',
   templateUrl: './multi-select.component.html',
-  styleUrls: ['./multi-select.component.scss']
+  styleUrls: ['./multi-select.component.scss'],
 })
 export class MultiSelectComponent implements OnInit {
   separatorKeysCodes: number[] = [ENTER, COMMA];
   selectControl = new FormControl(''); //ParentForm??
- 
 
-  itemProvider : SelectModel[] = [];
-  selectedItems : SelectModel[] = [];
-  filteredItems : Observable<SelectModel[]>;
+  itemProvider: SelectModel[] = [];
+  selectedItems: SelectModel[] = [];
+  filteredItems: Observable<SelectModel[]>;
   subscription!: Subscription;
 
   @ViewChild('input') input!: ElementRef<HTMLInputElement>;
 
-  constructor(private supplierService : SupplierService) {
-
+  constructor(private supplierService: SupplierService) {
     this.filteredItems = this.selectControl.valueChanges.pipe(
       startWith(''),
-      map(item => (this.filterItems(item || '')))
-    )
-
-
+      map((item) => this.filterItems(item || ''))
+    );
   }
   ngOnInit(): void {
     //If content is in a database
-    this.subscription = this.supplierService.selectValues$.subscribe(x => {
+    this.subscription = this.supplierService.selectValues$.subscribe((x) => {
       this.itemProvider = x;
-    })
+    });
   }
 
-  remove(itemValue : string): void {
+  public removeItem(itemValue: string): void {
     const targetItem = this.getItem(itemValue);
     const index = this.selectedItems.indexOf(targetItem);
 
@@ -49,26 +45,24 @@ export class MultiSelectComponent implements OnInit {
     }
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
+  public selectItem(event: MatAutocompleteSelectedEvent): void {
     const targetItem = this.getItem(event.option.value);
-   
-    if(targetItem != undefined)
-      this.selectedItems.push(targetItem);
 
-     this.input.nativeElement.value = '';
-     this.selectControl.setValue(null);
+    if (targetItem != undefined) this.selectedItems.push(targetItem);
+
+    this.input.nativeElement.value = '';
+    this.selectControl.setValue(null);
   }
 
-  private filterItems(value: string) : SelectModel[]{
+  private filterItems(value: string): SelectModel[] {
     const target = value.toLowerCase();
 
-    return this.itemProvider.filter(x => x.name.toLowerCase().includes(target));
+    return this.itemProvider.filter((x) =>
+      x.name.toLowerCase().includes(target)
+    );
   }
 
-  private getItem(targetValue : string) : SelectModel {
-    return this.itemProvider.filter(x => x.value === targetValue)[0];
+  private getItem(targetValue: string): SelectModel {
+    return this.itemProvider.filter((x) => x.value === targetValue)[0];
   }
-
-  
-
 }
