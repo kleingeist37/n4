22ed365bac0b9c2a1a0e4f896adccfe5d46dd9a2
kleingeist37 <seiten@awiworks.com>
2023-02-05 21:43:18 +0100

    added deepString functionality

--- src/app/components/custom-select/custom-select.component.ts
+++ src/app/components/custom-select/custom-select.component.ts
@@ -1,3 +1,4 @@
+import { M } from '@angular/cdk/keycodes';
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { isObservable, Observable, Subscription } from "rxjs";
 
@@ -11,9 +12,17 @@ export class CustomSelectComponent implements OnInit,OnDestroy {
   @Input('input') options : any;
 
   @Input('asyncInput') asyncOptions!: Observable<any>;
-  @Input('asyncTargetProp') asyncProp : string = "";
+  @Input('asyncTargetProp') targetProperty : string = "";   //string analyzer?? 
+  @Input('splitString') splitString: string = "";
+
+  @Input('multiSelect') allowMultiSelect : boolean = false;
 
   private sub!: Subscription;
+
+  private selectedOptions : string[] = [];
+
+
+  private hasAnalyzed = false;
 
   constructor() { }
   ngOnDestroy(): void {
@@ -24,32 +33,78 @@ export class CustomSelectComponent implements OnInit,OnDestroy {
 
   ngOnInit(): void {
     if(isObservable(this.asyncOptions)){
-      console.log("bla");
       this.sub = this.asyncOptions.subscribe((data : any) =>{
 
-        if(this.asyncProp.length > 0){
+        if(this.targetProperty.length > 0){
           //analyze object here
           //if string -> convert object wie in first draft ...
           //for debugPurposes      
 
-          this.options = data[0][this.asyncProp];
-          // let targetData = data[0][this.asyncProp] as string[];
-          // for(let i = 0; i < targetData.length; i++){
-          //   this.options.push(data[i])
-          // }
-          //console.log(data[0][`${this.asyncProp}`]);
+          this.options = data; //[this.targetProperty];
+          //then add data ...
         }
         else{
           this.observableHandler(data);
         }
          
       })
+    }else {
+      if(this.targetProperty.length > 0){
+        this.options = this.options; //[this.targetProperty];
+      }
     }
+  }
+
+  //change observable stuff to analyze splitstring
+  private analyzeString(){
+    // this.options = this.options[0]; //debug
+    console.log(this.options);
+    let splitted = this.splitString.split('.');
+
+    if(splitted.length <= 1){
+      console.log("no deepString found");
+      return;
+    }
+
+
+    this.targetProperty = splitted[splitted.length - 1];
+    console.log("target: " + this.targetProperty);
+    splitted.splice(splitted.length - 1, 1);
+    console.log(splitted);
+    let newValue = this.options[0];
+    for(let i = 0; i < splitted.length; i++){   
+      console.log(i);  
+      console.log(newValue);
+      newValue = this.propertyRaid(newValue, splitted[i]);
+    }
+   
+     this.options = newValue;
+     console.log(this.options);
+    this.hasAnalyzed = true;
+  }
+
+  private propertyRaid(obj: any, property: string) : any {
+    console.log(property + " " + obj[property]);
+    return obj[property];
+  }
+
+  public selected(option : any){
+    // if(!this.allowMultiSelect && this.selectedOptions.includes(option)){
+    //   this.selectedOptions.splice(this.selectedOptions.indexOf(option), 1);
+    // }
+    // else {
+    //   this.selectedOptions.push(option);
+    // }
+    if(!this.hasAnalyzed)
+      this.analyzeString();
+    //console.log(this.selectedOptions);
   }
   
 
   private observableHandler(data : any){
     this.options = data;
+    //analyze and test
+    
   }
 
 }
